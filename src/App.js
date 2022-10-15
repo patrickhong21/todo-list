2a7f82aed82f763610815d9bcf8d6d79bcbaf326
caden <z4h0n@ugrad.cs.ubc.ca>
2022-10-15 14:46:00 -0700

    quotes done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import List from "./components/List";
+import axios from 'axios';
+
+
+
 
 function App() {
   const [todoNames, setTodoNames] = useState([]);
+
+  // start of quote thing
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const quoteAPI = async () => {
+    let quoteArray = [];
+    try {
+      const data = await axios.get("https://api.quotable.io/random");
+      quoteArray = data.data;
+    } catch (error) {
+      console.log(error)
+    }
+
+    try {
+      setQuote(quoteArray.content)
+      setAuthor(quoteArray.author)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    quoteAPI();
+  }, [])
+  // end of quote thing
 
   useEffect(() => {
     console.log("array updated");
@@ -15,8 +45,17 @@ function App() {
 
   return (
     <div className="App">
+      <div className="quotes">
+        {quote} - 
+        {author}
+      </div>
+
       <button onClick={onClick}>Click me</button>
       <h1>Hello World</h1>
+      <form>
+        <label>Task: <input type="text" name="taskName" /></label>
+        <input type="submit" value="Submit" />
+      </form>
       <List todoNames={todoNames}/>
     </div>
   );
