5622ef7a12ca0a92637618d9f4ff9e16bf505660
Anthony <anthonymai177@hotmail.com>
2022-10-15 12:44:48 -0700

    fetch quote progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import './App.css';
+import FetchQuote from "./quotesFetcher";
 
 function App() {
   return (
     <div className="App">
+      <FetchQuote />
       <h1>Hello World</h1>
     </div>
   );
